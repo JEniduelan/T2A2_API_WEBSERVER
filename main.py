a32da93413606484de73d358b7dce51602189d0d
JEniduelan <jeniduelan@gmail.com>
2024-07-11 15:17:42 +1000

    finished initial setup and dependencies

--- main.py
+++ main.py
@@ -1,1 +1,21 @@
-from flask
+import os
+from flask import Flask
+
+from init import db, ma, bcrypt, jwt
+
+
+def create_app():
+    app = Flask(__name__)
+
+    app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URL")
+
+    app.config["JWT_SECRET_KEY"] = os.environ.get("JWT_SECRET_KEY")
+
+    db.init_app(app)
+    ma.init_app(app)
+    bcrypt.init_app(app)
+    jwt.init_app(app)
+    
+    return app
+
+
