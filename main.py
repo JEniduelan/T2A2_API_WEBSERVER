ac931e113740e3474a2d37281834247212552bfb
JEniduelan <jeniduelan@gmail.com>
2024-07-12 19:35:16 +1000

    created user model and schema

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ def create_app():
     app = Flask(__name__)
 
     app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URL")
-
+    
     app.config["JWT_SECRET_KEY"] = os.environ.get("JWT_SECRET_KEY")
 
     db.init_app(app)
---
+++ models/user.py
@@ -0,0 +1,26 @@
+from init import db, ma
+from marshmallow import fields
+
+class User(db.Model):
+    __tablename__ = "users"
+    
+    user_id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, nullable=False)
+    email = db.Column(db.String, nullable=False, unique=True)
+    password = db.Column(db.String, nullable=False)
+    is_admin = db.Column(db.boolean, default=False)
+    
+    
+    
+    
+class UserSchema(ma.Schema):
+    
+    class Meta:
+        fields = ("user_id", "name", "email", "password", "is_admin")
+        
+
+user_schema = UserSchema(exlude=["password"])
+
+users_schema = UserSchema(many=True, exclude=["password"])
+    
+    
